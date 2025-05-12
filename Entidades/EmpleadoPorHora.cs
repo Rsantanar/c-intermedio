eb12d111789c7ecd91172983f6bfe1f09a4d9b6d
rasantana <rodrisant25@gmail.com>
2025-05-12 02:55:11 -0400

    guardado

---
+++ Entidades/EmpleadoPorHora.cs
@@ -0,0 +1,23 @@
+﻿
+namespace GestionNomina.Entidades
+{
+    public  class EmpleadoPorHora: Empleado
+    {
+        public decimal PagoHora {  get; set; }  
+        public decimal HorasTrabajada {  get; set; }    
+
+        public EmpleadoPorHora(string codigo,string nombre,decimal pagohora,decimal horastrabajada)
+            :base (codigo,nombre)
+        {
+            PagoHora=pagohora;
+            HorasTrabajada=horastrabajada;
+        }
+        public override decimal CalcularSalario()
+        {
+            if (HorasTrabajada < 40)
+                return HorasTrabajada * PagoHora;
+            else
+                return (40 * PagoHora) + ((HorasTrabajada - 40) * PagoHora * 1.5m);
+        }
+    }
+}
